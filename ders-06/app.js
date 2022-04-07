4361445e3e393c917e01fc43dbe8284f39d9f8dc
Zehra ikizler <zehraaikizler@gmail.com>
2022-04-07 20:29:58 +0300

    ders-06 completed

---
+++ ders-06/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            memberList: [
+                { id: 1, name: "1. Kişi" },
+                { id: 2, name: "2. Kişi" },
+                { id: 3, name: "3. Kişi" },
+                { id: 4, name: "4. Kişi" },
+                { id: 5, name: "5. Kişi" },
+                { id: 6, name: "6. Kişi" },
+                { id: 7, name: "7. Kişi" },
+                { id: 8, name: "8. Kişi" },
+                { id: 9, name: "9. Kişi" },
+                { id: 10, name: "10. Kişi" },
+              ],
+        }
+    }
+}).mount("#app");
