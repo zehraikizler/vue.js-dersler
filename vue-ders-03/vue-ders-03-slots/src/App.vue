457d3d6650a08b8da900a17f81e13132742b8432
Zehra ikizler <zehraaikizler@gmail.com>
2022-04-18 14:05:25 +0300

    vue-ders-03-slots added

---
+++ vue-ders-03/vue-ders-03-slots/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- <ModalComp :onSave="onSave" :title="'ModalComp Title'" :content="content" /> -->
+  <ModalComp>
+    <template #title>
+      <h3>Slot ile Gelen Title Bilgisi</h3>
+    </template>
+
+    <template v-slot:content>
+      <p class="text-success">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis iusto nobis odit aliquid cupiditate, in beatae eum tempora rem id
+        ipsum aliquam, accusamus rerum numquam? Tempora optio itaque dicta!
+      </p>
+    </template>
+    <!-- <template #content>
+      <p class="text-success">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro veritatis iusto nobis odit aliquid cupiditate, in beatae eum tempora rem id
+        ipsum aliquam, accusamus rerum numquam? Tempora optio itaque dicta!
+      </p>
+    </template> -->
+
+    <template #default>
+      Bu default olarak gösterilecek..
+    </template>
+  </ModalComp>
+</template>
+
+<script>
+import ModalComp from "@/components/ModalComp";
+export default {
+  components: {
+    ModalComp
+  },
+  data() {
+    return {
+      content: `<h3 class="text-danger">title</h3>`
+    };
+  },
+  methods: {
+    onSave() {
+      alert("Saved!!");
+    }
+  }
+};
+</script>
